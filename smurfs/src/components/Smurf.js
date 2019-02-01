765a2f3da9f9fef1d8329c3c36d727c02f2cbb60
Mike Landers <landers.mike@gmail.com>
2019-02-01 11:01:20 -0800

    basic styling

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Smurf(props) {
 	return (
-		<iv className="smurf">
+		<div className="smurf">
 			<h1>{props.smurf.name}</h1>
 			<p>Age: {props.smurf.age}</p> <p>Height: {props.smurf.height}</p>
 			<button onClick={e => props.handleDelete(e, props.smurf.id)}>
@@ -11,7 +11,7 @@ function Smurf(props) {
 			<button onClick={e => props.handleEdit(e, props.smurf.id)}>
 				Edit
 			</button>
-		</iv>
+		</div>
 	);
 }
 
